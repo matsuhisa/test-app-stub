f61a1f7ec2ad5b1ff032df6b9dc8f8e0dfa88cfc
matsuhisa <matsuhisa+github@gmail.com>
2017-11-29 00:01:41 +0900

    Update features

--- spec/features/books_spec.rb
+++ spec/features/books_spec.rb
@@ -1,9 +1,19 @@
 require 'rails_helper'
 
 RSpec.feature 'books', type: :feature do
-  scenario '#index' do
-    visit books_path
+  context '#index' do
+    scenario '' do
+      visit books_path
+      expect(page).to have_http_status(200)
+    end
+  end
 
-    expect(page).to have_http_status(200)
+  context '#show' do
+    let(:book) { create(:book) }
+
+    it "" do
+      visit book_path book
+      expect(page).to have_http_status(200)
+    end
   end
 end
